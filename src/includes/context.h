3bb6fbdad09f12dff4ff9cc9dc266cf8be87ea62
Jelle Van snik <jvan-sni@f0r4s5.codam.nl>
2022-03-31 15:09:54 +0200

    basic setup

---
+++ src/includes/context.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*   Cats are the best!                                          _ |\_        */
+/*   And forced headers are stupid                               \` ..\       */
+/*                                                          __,.-" =__Y=      */
+/*   By: the cats                                         ."        )         */
+/*                                                  _    /   ,    \/\_        */
+/*   Created: // :: by                             ((____|    )_-\ \_-`       */
+/*   Updated: // :: by                             `-----'`-----` `--`        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTEXT_H
+# define CONTEXT_H
+
+/*
+** Create a FTLS context.
+** this is used throughout the entire program and stores all options
+** that are passed in
+*/
+
+int	create_context(void);
+
+#endif
