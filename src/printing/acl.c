0060d722da1e6084a22f7f0e785eb101437e8f8d
Jelle van Snik <jellevs@gmail.com>
2022-05-04 13:11:42 +0200

    implement acl fully

--- src/printing/acl.c
+++ src/printing/acl.c
@@ -6,10 +6,49 @@ static char	get_acl_dir(ftls_file_info *file) {
 		return 'l';
 	if (S_ISDIR(file->stat.st_mode))
 		return 'd';
+	if (S_ISCHR(file->stat.st_mode))
+		return 'c';
+	if (S_ISFIFO(file->stat.st_mode))
+		return 'p';
+	if (S_ISBLK(file->stat.st_mode))
+		return 'b';
+	if (S_ISSOCK(file->stat.st_mode))
+		return 's';
 	return '-';
 }
 
-// TODO extended attributes + special chars https://www.unix.com/unix-for-dummies-questions-and-answers/41832-ls-l-column-headings.html
+static char	get_acl_exec_user(ftls_file_info *file) {
+	if ((file->stat.st_mode & ( S_ISUID | S_IXUSR )) == (S_ISUID | S_IXUSR))
+		return 's';
+	if (file->stat.st_mode & S_ISUID)
+		return 'S';
+	if (file->stat.st_mode & S_IXUSR)
+		return 'x';
+	return '-';
+}
+
+static char	get_acl_exec_group(ftls_file_info *file) {
+	if ((file->stat.st_mode & ( S_ISGID | S_IXGRP )) == (S_ISGID | S_IXGRP))
+		return 's';
+	if (file->stat.st_mode & S_ISGID)
+		return 'S';
+	if (file->stat.st_mode & S_IXGRP)
+		return 'x';
+	return '-';
+}
+
+static char	get_acl_exec_other(ftls_file_info *file) {
+	if ((file->stat.st_mode & ( S_ISVTX | S_IXOTH )) == (S_ISVTX | S_IXOTH))
+		return 't';
+	if (file->stat.st_mode & S_ISVTX)
+		return 'T';
+	if (file->stat.st_mode & S_IXOTH)
+		return 'x';
+	return '-';
+}
+
+// TODO extended attributes
+// TODO follow directory if one arg (no trailing slash)
 char	*get_acl(ftls_file_info *file) {
 	char *out = malloc(12);
 	if (!out)
@@ -20,17 +59,17 @@ char	*get_acl(ftls_file_info *file) {
 	// user RWX
 	out[i++] = file->stat.st_mode & S_IRUSR ? 'r' : '-';
 	out[i++] = file->stat.st_mode & S_IWUSR ? 'w' : '-';
-	out[i++] = file->stat.st_mode & S_IXUSR ? 'x' : '-';
+	out[i++] = get_acl_exec_user(file);
 
 	// group RWX
 	out[i++] = file->stat.st_mode & S_IRGRP ? 'r' : '-';
 	out[i++] = file->stat.st_mode & S_IWGRP ? 'w' : '-';
-	out[i++] = file->stat.st_mode & S_IXGRP ? 'x' : '-';
+	out[i++] = get_acl_exec_group(file);
 
 	// other RWX
 	out[i++] = file->stat.st_mode & S_IROTH ? 'r' : '-';
 	out[i++] = file->stat.st_mode & S_IWOTH ? 'w' : '-';
-	out[i++] = file->stat.st_mode & S_IXOTH ? 'x' : '-';
+	out[i++] = get_acl_exec_other(file);
 
 	out[i++] = 0;
 	return out;
