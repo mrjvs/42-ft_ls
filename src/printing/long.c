85daf0d4c10175d82a652550c1d44820cb4a3ed0
Jelle van Snik <jellevs@gmail.com>
2022-05-18 16:06:06 +0200

    proper total number

--- src/printing/long.c
+++ src/printing/long.c
@@ -254,8 +254,13 @@ void			print_long_format(ftls_context *ctx, ftls_dir *dir, ftls_print_options op
 		{
 			ftls_file_info file = get_list_data(lst, struct s_ftls_dir_entry)->file;
 
-			if (should_print_file(ctx, &file))
-				blocks += file.stat.st_blocks / 2; // stat gives 512 blocks, we need to display 1024 blocks
+			if (should_print_file(ctx, &file)) {
+# ifdef MACOS
+				blocks += file.stat.st_blocks;
+# else
+				blocks += file.stat.st_blocks / 2; // stat on linux gives 512 blocks, we need to display 1024 blocks
+# endif
+			}
 		}
 		ftls_write(STDOUT_FILENO, "total ");
 		char *block_str = ftls_ltoa(blocks);
